8bc9a6dfb15cf63434293f2557707ff4d599d962
Ohjun <ohjuni89@gmail.com>
2023-01-10 09:26:42 +0900

    1-4 starter build

---
+++ RxSwift-Kodeco/Combinestagram/Classes/PhotoWriter.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoWriter.swift
+//  RxSwift-Kodeco
+//
+//  Created by Ohjun-Wizardlab on 2023/01/06.
+//
+
+import Foundation
+import UIKit
+import Photos
+
+class PhotoWriter {
+  enum Errors: Error {
+    case couldNotSavePhoto
+  }
+
+
+}
---
+++ RxSwift-Kodeco/Combinestagram/PhotoCell.swift
@@ -0,0 +1,27 @@
+//
+//  PhotoCell.swift
+//  RxSwift-Kodeco
+//
+//  Created by Ohjun-Wizardlab on 2023/01/06.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+
+  @IBOutlet var imageView: UIImageView!
+  var representedAssetIdentifier: String!
+
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    imageView.image = nil
+  }
+
+  func flash() {
+    imageView.alpha = 0
+    setNeedsDisplay()
+    UIView.animate(withDuration: 0.5, animations: { [weak self] in
+      self?.imageView.alpha = 1
+    })
+  }
+}
